7e555a50d5607412726703e5f649992b9834dee0
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2024-07-17 14:44:40 +0000

    introduced styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import styled from 'styled-components'
 
+const H1 = styled.h1`
+color: blue;
+font-size: 1.5rem;
+`
 export default function App() {
   return (
     <main>
-    <h1>
-      React ⚛️ + Vite ⚡ + Replit</h1>
+    <H1>
+      React ⚛️ + Vite ⚡ + Replit</H1>
       <p>Test</p>
     </main>
   )
