203213d565e4a2aa55fccf6c24bea057c6a3cc02
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2024-07-17 13:42:40 +0000

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import './App.css'
 export default function App() {
   return (
     <main>
-      React ⚛️ + Vite ⚡ + Replit
+    <h1>
+      React ⚛️ + Vite ⚡ + Replit</h1>
+      <p>Test</p>
     </main>
   )
 }
